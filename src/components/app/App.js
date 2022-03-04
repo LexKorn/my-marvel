0ad9b0d760800db009b322982239d69882ca036b
Lex <fischer84@mail.ru>
2022-03-04 20:14:38 +0300

    init

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+
+function App() {
+  return (
+    <div className="app">
+      <AppHeader />
+      <RandomChar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appBaner/AppBaner.js
@@ -0,0 +1,16 @@
+import './appBaner.sass';
+
+const AppBaner = () => {
+    return (
+        <div className="baner">
+            <div className="random-char">
+
+            </div>
+            <div className="cta-block">
+
+            </div>
+        </div>
+    )
+}
+
+export default AppBaner;
