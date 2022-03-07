16615b36b6c5aabf7550609c437aa638f39e4e25
Lex <fischer84@mail.ru>
2022-03-07 08:56:20 +0300

    edited RandomCahr and MarvelService

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+// import MarvelService from './services/MarvelService';
 
 import './style/style.sass';
 
+/*
 const marvelService = new MarvelService();
 
-// marvelService.getAllCharacters().then(res => console.log(res));  
+marvelService.getAllCharacters().then(res => console.log(res));  
 marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name, item.id))); 
+marvelService.getCharacter(1009225).then(res => console.log(res));
+*/
 
-// marvelService.getCharacter(1009225).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
